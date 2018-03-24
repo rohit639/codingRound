c12a69d60ab90d1680f06058f174dfaf1787dd47
LENOVO\USER <rohit639u@gmail.com>
2018-03-24 14:04:16 +0530

    HotelBookingTest
    1. Driver should be initialized after setting platform
    2. need to set PageFactory in static block
    3. need to select option provided in location where
    4. date selecton
    5. need to check next page validation -  Indiranagar

--- src/main/java/HotelBookingTest.java
+++ src/main/java/HotelBookingTest.java
@@ -1,14 +1,20 @@
 import com.sun.javafx.PlatformUtil;
+
+import java.util.List;
+
+import org.openqa.selenium.By;
+import org.openqa.selenium.Keys;
 import org.openqa.selenium.WebDriver;
 import org.openqa.selenium.WebElement;
 import org.openqa.selenium.chrome.ChromeDriver;
 import org.openqa.selenium.support.FindBy;
+import org.openqa.selenium.support.PageFactory;
 import org.openqa.selenium.support.ui.Select;
 import org.testng.annotations.Test;
 
 public class HotelBookingTest {
 
-    WebDriver driver = new ChromeDriver();
+    WebDriver driver;
 
     @FindBy(linkText = "Hotels")
     private WebElement hotelLink;
@@ -25,15 +31,38 @@ public class HotelBookingTest {
     @Test
     public void shouldBeAbleToSearchForHotels() {
         setDriverPath();
-
+        driver = new ChromeDriver();
+        PageFactory.initElements(driver, this);
         driver.get("https://www.cleartrip.com/");
         hotelLink.click();
 
         localityTextBox.sendKeys("Indiranagar, Bangalore");
-
+        try {
+			Thread.sleep(2000);
+		} catch (Exception e) {
+			// TODO: handle exception
+		}
+        localityTextBox.sendKeys(Keys.RETURN);
+       // driver.findElement(By.xpath("//*[@id='ui-datepicker-div']/div[1]/table/tbody/tr[4]/td[7]")).click();
+        try {
+			Thread.sleep(2000);
+		} catch (Exception e) {
+			// TODO: handle exception
+		}
+        driver.findElement(By.xpath("//div[@class='monthBlock first']//tr[5]//td[1]")).click();
+        try {
+			Thread.sleep(2000);
+		} catch (Exception e) {
+			// TODO: handle exception
+		}
+        driver.findElement(By.xpath("//div[@class='monthBlock first']//tr[5]//td[2]")).click();
         new Select(travellerSelection).selectByVisibleText("1 room, 2 adults");
         searchButton.click();
-
+        try {
+			Thread.sleep(2000);
+		} catch (Exception e) {
+			// TODO: handle exception
+		}
         driver.quit();
 
     }
@@ -42,12 +71,13 @@ public class HotelBookingTest {
         if (PlatformUtil.isMac()) {
             System.setProperty("webdriver.chrome.driver", "chromedriver");
         }
-        if (PlatformUtil.isWindows()) {
+        else if (PlatformUtil.isWindows()) {
             System.setProperty("webdriver.chrome.driver", "chromedriver.exe");
         }
-        if (PlatformUtil.isLinux()) {
+        else if (PlatformUtil.isLinux()) {
             System.setProperty("webdriver.chrome.driver", "chromedriver_linux");
         }
+        else System.out.println("Failed as only Mac, Windows and, linux is supported ");
     }
 
 }
