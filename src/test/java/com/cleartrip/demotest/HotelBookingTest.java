a594363158637237671136e89210a506ef83a935
LENOVO\USER <rohit639u@gmail.com>
2018-03-25 18:45:09 +0530

    Changes Methods based on Pages. Next step would be to use all test cases in 1 class and define locators and date function

--- src/test/java/com/cleartrip/demotest/HotelBookingTest.java
+++ src/test/java/com/cleartrip/demotest/HotelBookingTest.java
@@ -1,74 +1,16 @@
 package com.cleartrip.demotest;
-import com.cleartrip.utils.BaseWebdriver;
-import com.cleartrip.utils.FunctionLib;
-import org.openqa.selenium.By;
-import org.openqa.selenium.Keys;
-import org.openqa.selenium.WebElement;
-import org.openqa.selenium.support.FindBy;
-import org.openqa.selenium.support.PageFactory;
-import org.testng.Assert;
 import org.testng.annotations.Test;
 
+import com.cleartrip.pages.LandingPage;
+import com.cleartrip.utils.BaseWebdriver;
+
 public class HotelBookingTest extends BaseWebdriver {
-
-   
-
-    @FindBy(linkText = "Hotels")
-    private WebElement hotelLink;
-
-    @FindBy(id = "Tags")
-    private WebElement localityTextBox;
-
-    @FindBy(id = "SearchHotelsButton")
-    private WebElement searchButton;
-
-    @FindBy(id = "travellersOnhome")
-    private WebElement travellerSelection;
-
-    public HotelBookingTest() {
-    	
-	}
     
     @Test
     public void shouldBeAbleToSearchForHotels() {
-    	PageFactory.initElements(getDriver(), this);
-    	
-        hotelLink.click();
-
-        localityTextBox.sendKeys("Indiranagar, Bangalore");
-        try {
-			Thread.sleep(2000);
-		} catch (Exception e) {
-			// TODO: handle exception
-		}
-        
-        localityTextBox.sendKeys(Keys.RETURN);
-       
-        
-        WebElement dateFrom = driver.findElement(By.xpath("//div[@class='monthBlock first']//tr[5]//td[1]"));
-        Assert.assertTrue((FunctionLib.isElemntVisble(dateFrom)),"Failed as not able to view From Date for hotel booking");
-        dateFrom.click();
-        
-        
-        WebElement checkOutDate = driver.findElement(By.id("CheckOutDate"));
-        Assert.assertTrue((FunctionLib.isElemntVisble(checkOutDate)),"Failed as not able to view to check out Date for hotel booking");
-        checkOutDate.click();
-        WebElement dateTill = driver.findElement(By.xpath("//*[@id='ui-datepicker-div']/div[2]/table/tbody/tr[4]/td[7]"));
-       Assert.assertTrue((FunctionLib.isElemntVisble(dateTill)),"Failed as not able to view To Date for hotel booking");
-       dateTill.click();
-       
-        Assert.assertTrue(FunctionLib.seletBasedOnVisibleText(travellerSelection, "1 room, 2 adults"));
-        
-        searchButton.click();
-        try {
-			Thread.sleep(2000);
-		} catch (Exception e) {
-			// TODO: handle exception
-		}
-      
-
+    	LandingPage homePage = new LandingPage();
+    	homePage.navigateToHotelPage().navigateToHotelSearchPage();
     }
-
    
 
 }
--- src/test/java/com/cleartrip/demotest/SignInTest.java
+++ src/test/java/com/cleartrip/demotest/SignInTest.java
@@ -1,13 +1,9 @@
 package com.cleartrip.demotest;
-import com.cleartrip.utils.BaseWebdriver;
-import com.cleartrip.utils.FunctionLib;
-import com.sun.javafx.PlatformUtil;
-import org.openqa.selenium.By;
-import org.openqa.selenium.WebDriver;
-import org.openqa.selenium.WebElement;
-import org.openqa.selenium.chrome.ChromeDriver;
 import org.testng.Assert;
 import org.testng.annotations.Test;
+
+import com.cleartrip.pages.LandingPage;
+import com.cleartrip.utils.BaseWebdriver;
 
 public class SignInTest extends BaseWebdriver {
 
@@ -16,25 +12,11 @@ public class SignInTest extends BaseWebdriver {
     @Test
     public void shouldThrowAnErrorIfSignInDetailsAreMissing() {
 
-         WebElement yourTrips = driver.findElement(By.linkText("Your trips"));
-         Assert.assertTrue(FunctionLib.isElemntVisble(yourTrips), "Failed as not able to view your trips link");
-         yourTrips.click();
-         WebElement singIn = driver.findElement(By.id("SignIn"));
-         Assert.assertTrue(FunctionLib.isElemntVisble(singIn), "Failed as not able to view Sign-In button link");
-         singIn.click();
-         
-        WebElement frame = driver.findElement(By.id("modal_window"));
-         Assert.assertTrue(FunctionLib.switchToFrame(frame));
+    	 LandingPage homePage = new LandingPage();
         
-        WebElement formSignIn =  driver.findElement(By.id("signInButton"));
-        Assert.assertTrue(FunctionLib.isElemntVisble(formSignIn), "Failed as not able to view sign-In in sign-In pagr/form");
-        formSignIn.click();
-         String errors1 = driver.findElement(By.id("errors1")).getText();
-        
-         Assert.assertTrue(errors1.contains("There were errors in your submission"));
+         Assert.assertTrue(homePage.navigateToSignInPage().verifySignIn(),"Failed to verify error message for SignIn");
          driver.switchTo().parentFrame();
-         
-    }
+     }
 
    
 
